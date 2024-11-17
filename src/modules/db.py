bc7dd823334cebf83135b024121abdf619b90622
Frank3r <frarantas@gmail.com>
2024-11-17 20:24:19 +0100

    upd(db): schema

--- src/modules/db.py
+++ src/modules/db.py
@@ -11,4 +11,56 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(db.String(128), unique=True)
     username: Mapped[str] = mapped_column(db.String(128))
-    password_hash: Mapped[str] = mapped_column(db.String(24))
+    password_hash: Mapped[str] = mapped_column(db.String(60))
+    role: Mapped[int] = mapped_column(db.Integer)
+    avatar_path: Mapped[str] = mapped_column(db.String(128))
+    blocked: Mapped[bool] = mapped_column(db.Boolean)
+
+class Group(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(db.String(128))
+    owner_id: Mapped[int] = mapped_column(ForeignKey(User.id))
+    description: Mapped[str] = mapped_column(db.String(256))
+    avatar_path: Mapped[str] = mapped_column(db.String(128))
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str] = mapped_column(db.String(256))
+    post_date: Mapped[str] = mapped_column(db.DateTime)
+    file_path: Mapped[str] = mapped_column(db.String(256))
+    visibility: Mapped[bool] = mapped_column(db.Boolean)
+
+class Tag(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(db.String(128))
+    blocked: Mapped[bool] = mapped_column(db.Boolean)
+
+class PostTag(db.Model):
+    post_id: Mapped[int] = mapped_column(ForeignKey(Post.id),primary_key=True,)
+    tag_id: Mapped[int] = mapped_column(ForeignKey(Tag.id),primary_key=True,)
+
+class PostGroup(db.Model):
+    post_id: Mapped[int] = mapped_column(ForeignKey(Post.id),primary_key=True,)
+    group_id: Mapped[int] = mapped_column(ForeignKey(Group.id),primary_key=True,)
+
+class UserGroup(db.Model):
+    user_id: Mapped[int] = mapped_column(ForeignKey(User.id),primary_key=True,)
+    group_id: Mapped[int] = mapped_column(ForeignKey(Group.id),primary_key=True,)
+
+class GroupInvite(db.Model):
+    user_id: Mapped[int] = mapped_column(ForeignKey(User.id), primary_key=True, )
+    group_id: Mapped[int] = mapped_column(ForeignKey(Group.id), primary_key=True, )
+    user_pending: Mapped[bool] = mapped_column(db.Boolean)
+    group_pending: Mapped[bool] = mapped_column(db.Boolean)
+    date_created: Mapped[str] = mapped_column(db.DateTime)
+
+class Score(db.Model):
+    post_id: Mapped[int] = mapped_column(ForeignKey(Post.id),primary_key=True,)
+    user_id: Mapped[int] = mapped_column(ForeignKey(User.id),primary_key=True,)
+    score: Mapped[bool] = mapped_column(db.Boolean)
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    post_id: Mapped[int] = mapped_column(ForeignKey(Post.id))
+    user_id: Mapped[int] = mapped_column(ForeignKey(User.id))
+    content: Mapped[str] = mapped_column(db.String(256))
