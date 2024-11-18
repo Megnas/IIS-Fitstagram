b2d74525a4d268099358b0923dfa48e8d7357982
Frank3r <frarantas@gmail.com>
2024-11-19 00:04:22 +0100

    fix(app): added user namanger module.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
-
 from modules.db import db
+import modules.user_manager as um
 
 app = Flask(__name__)
 
@@ -14,14 +14,11 @@ print("Db: ", db_path)
 #Set db param (if env does not exist, will default to "sqlite:///project.db")
 app.config["SQLALCHEMY_DATABASE_URI"] = db_path if db_path else "sqlite:///project.db"
 
-
 with  app.app_context():
     db.init_app(app)
     db.create_all()
 
-@app.route("/")
-def main():
-    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
