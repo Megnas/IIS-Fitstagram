fe286bcaa644b920cb4fd3eb12de75ca8437e6e1
Megnas <vaclavikdom@gmail.com>
2024-11-19 01:43:14 +0100

    fix: post merge fix

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 from modules.db import db
+from modules.auth import init_login_manager
 
 app = Flask(__name__)
 
@@ -13,6 +14,8 @@ print("Db: ", db_path)
 #Set db param (if env does not exist, will default to "sqlite:///project.db")
 app.config["SQLALCHEMY_DATABASE_URI"] = db_path if db_path else "sqlite:///project.db"
 
+#TODO: Load secret key from .env
+app.config['SECRET_KEY'] = 'not a secret'
 
 with  app.app_context():
     db.init_app(app)
@@ -23,7 +26,9 @@ with  app.app_context():
 from modules import auth_template
 app.register_blueprint(auth_template.bp)
 
-
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
