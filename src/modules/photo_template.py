2bdd4380dba5c6ae40c8059e74c041312fa79b1f
Megnas <vaclavikdom@gmail.com>
2024-11-23 02:25:45 +0100

    feat: updated group_photo

--- src/modules/photo_template.py
+++ src/modules/photo_template.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, send_file, abort
+from flask_login import current_user
 import io
 
 from .user_manager import get_user
 from .photo_manager import get_pic_by_id
+from .groups_manager import get_group
 
 bp = Blueprint('foto', __name__)
 
@@ -32,4 +34,29 @@ def get_user_image(user_id):
 
 @bp.route('/group_image/<int:group_id>')
 def get_group_image(group_id):
-    pass
+    group = get_group(group_id)
+
+    if not group.visibility:
+        if not current_user.is_authenticated:
+            abort(401, description="Not authorized!")
+
+    if group:
+        if(group.photo_id):
+            image = get_pic_by_id(group.photo_id)
+            if(image):
+                #Return image
+                return send_file(
+                    io.BytesIO(image.data),
+                    mimetype=image.mimetype,
+                    as_attachment=False,
+                    download_name=image.name
+                )
+            else:
+                #Return error missing image
+                abort(404, description="Image data not found.")
+        else:
+            #Return place holder
+            return send_file("static/images/user_placeholder.jpg", mimetype="image/jpg")
+    else:
+        #Return error non-existent group
+        abort(404, description="Group does not exists.")
